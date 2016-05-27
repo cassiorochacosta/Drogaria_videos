16ed7e6a496499783cf090c8a7065fe6cc59bff5
Cassio Rocha <cassiorochacosta@hotmail.com>
2016-05-27 12:37:02 -0400

    implementando classes DAO

---
+++ src/main/java/br/pro/delfino/drogria/dao/CidadeDAO.java
@@ -0,0 +1,7 @@
+package br.pro.delfino.drogria.dao;
+
+import br.pro.delfino.drogria.domain.Cidade;
+
+public class CidadeDAO extends GenericDAO<Cidade> {
+
+}
---
+++ src/main/java/br/pro/delfino/drogria/dao/ProdutoDAO.java
@@ -0,0 +1,7 @@
+package br.pro.delfino.drogria.dao;
+
+import br.pro.delfino.drogria.domain.Cidade;
+
+public class ProdutoDAO extends GenericDAO<Cidade> {
+
+}
